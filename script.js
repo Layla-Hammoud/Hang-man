0428715a75fb33f0204e34960344423574bace48
Layla Hammoud <laylahammoud63@gmail.com>
2023-02-27 22:25:19 +0200

    create the get random item function

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
+let guess_number = 6;
 const words = [
     {
-      word: "javascript",
-      hint: "A programming language often used in web development"
+        word: "javascript",
+        hint: "A programming language often used in web development"
     },
     {
-      word: "pizza",
-      hint: "A popular Italian dish often topped with cheese and tomato sauce"
+        word: "pizza",
+        hint: "A popular Italian dish often topped with cheese and tomato sauce"
     },
     {
-      word: "beach",
-      hint: "A sandy area near the ocean or lake, often used for recreational activities"
+        word: "beach",
+        hint: "A sandy area near the ocean or lake, often used for recreational activities"
     },
     {
-      word: "mountain",
-      hint: "A high landform, often rocky and covered with snow, located above the surrounding area"
+        word: "mountain",
+        hint: "A high landform, often rocky and covered with snow, located above the surrounding area"
     },
     {
-      word: "guitar",
-      hint: "A musical instrument with strings that is often used in rock, pop, and folk music"
+        word: "guitar",
+        hint: "A musical instrument with strings that is often used in rock, pop, and folk music"
     },
-  ];
+];
+
+const game = () =>{
+
+}
+const getRandomItem = (words) =>{
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const item = words[randomIndex];
+    return item;
+}
+console.log(getRandomItem(words))
